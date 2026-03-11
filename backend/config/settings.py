54188ee6574af60ed9996219dd5825255748c83c
Juliannn <flbloodheist@gmail.com>
2026-03-11 23:45:36 +0800

    postgresql config commented

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -91,6 +91,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'microblend',
+#         'USER': 'postgres',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
